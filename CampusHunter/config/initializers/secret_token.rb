9cc0c95b577b99fb58870022ee7cc3385aa51ec8
shilpa <shilpas@thoughtworks.com>
2012-10-05 10:37:00 +0530

    isha/shilpa project structure

---
+++ CampusHunter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CampusHunter::Application.config.secret_token = '1e83c538133051fe07ac0d015a5d2d9ad2457f852f15c2e67dc03a9be5e5fe3f36341f4335666fe4e53c867b5c54351e992deaf59f1c0f7eeaad51d9ffdfec61'
