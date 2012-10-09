9ccbe4e2c5ff5f10e836e1088c398d6d661e3dfc
shilpa <shilpas@thoughtworks.com>
2012-10-09 14:56:12 +0530

    dghfj

--- test/functional/applicant_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ApplicantControllerTest < ActionController::TestCase
-  test "should get show_details" do
-    get :show_details
-    assert_response :success
-  end
-
-end
