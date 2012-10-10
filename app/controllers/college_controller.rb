86a3e3a5975e8256b277e7aa513ec1d4ce315c35
shilpa <shilpas@thoughtworks.com>
2012-10-10 17:13:03 +0530

    shilpa/arnav file imported successfully

--- app/controllers/college_controller.rb
+++ app/controllers/college_controller.rb
@@ -7,8 +7,10 @@ class CollegeController < ApplicationController
   def new
 
     Rake::Task["db:load_csv_data"].reenable
-    Rake::Task["db:load_csv_data"].invoke
+    Rake::Task["db:load_csv_data"].invoke('Sample.csv')
+
   end
 
+  helper_method :new
 
 end
