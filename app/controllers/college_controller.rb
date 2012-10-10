97a3d93db44584cec45b5f274078efad71142547
shilpa <shilpas@thoughtworks.com>
2012-10-10 14:29:35 +0530

    shilpa/arnav- Can now upload a .csv file and update database.

--- app/controllers/college_controller.rb
+++ app/controllers/college_controller.rb
@@ -1,7 +1,14 @@
+require 'rake'
+Rake::Task.clear
+CampusHunter::Application.load_tasks
+
 class CollegeController < ApplicationController
 
   def new
 
+    Rake::Task["db:load_csv_data"].reenable
+    Rake::Task["db:load_csv_data"].invoke
   end
+
 
 end
