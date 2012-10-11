35220ea03b013131ed158c3cf5aa0dc08d23d1cd
isha <thisisisha@github.com>
2012-10-11 16:40:08 +0530

    schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,6 @@ ActiveRecord::Schema.define(:version => 20121010103427) do
   create_table "users", :force => true do |t|
     t.string   "email"
     t.string   "password"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
 end
