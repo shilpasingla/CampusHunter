cad0531a5dd0507ea480a45460b2dae51a234a20
isha <thisisisha@github.com>
2012-10-11 17:10:51 +0530

    Isha/Arnav -Encryption working

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 
   def self.authenticate(email,password)
     user = find_by_email(email)
-    if user && user.password == BCrypt::Engine.hash_secret(password, BCrypt::Engine.generate_salt)
+    if user && user.password == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
     else
       nil
@@ -17,6 +17,7 @@ class User < ActiveRecord::Base
   validates :password, :length => { :in => 4..20 }
 
   def encrypt_password
-    self.password = BCrypt::Engine.hash_secret(password, BCrypt::Engine.generate_salt)
+    self.password_salt = BCrypt::Engine.generate_salt
+    self.password = BCrypt::Engine.hash_secret(password, password_salt)
   end
 end
