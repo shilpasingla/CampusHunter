a9210399bceeb4c699cf293b1ba8df8f4b5aefee
isha <thisisisha@github.com>
2012-10-11 12:18:41 +0530

    continuing validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password
+  attr_accessible :email, :password, :password_confirmation
 
   def self.authenticate(email,password)
     user = find_by_email(email)
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
     end
   end
   validates :email, :password, :presence => true
+  validates :password, :confirmation => {:message => "and Password_confirmation should be same"}
   validates :email, :uniqueness => true
   validates :password, :length => { :in => 4..20 }
 end
