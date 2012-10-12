4df3c89bea0057be672e24173dcab4910f3baf2a
arnav90 <arnavk90@thoughtworks.com>
2012-10-12 15:50:54 +0530

    Arnav - Adding Unit tests for User

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
+
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Email should not be empty" do
+    user = User.new(password:"abcd")
+    assert user.invalid?
+
+  end
+
+  test "Password should not be empty" do
+    user = User.new(email:"random")
+    assert user.invalid?
+
+  end
+
+  test "Email should not be empty string" do
+    user=User.new(email:"    ",password:"abcd")
+    assert_equal(true, user.invalid?)
+
+  end
+
+  test "Password should not be empty string" do
+    user=User.new(email:"random",password:"    ")
+    assert_equal(true,user.invalid?)
+
+  end
+
 end
