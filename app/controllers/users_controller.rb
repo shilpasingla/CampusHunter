0902b4aa20062dc4ec2724000e77f117b76cd592
vinay mor <vinaymor002@gmail.com>
2012-10-16 11:38:56 +0530

    arnav/vinay : moved require login to application_controller , added validation for csv file  in college_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,15 +13,6 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-
-  def require_login
-    if session[:user_id] == nil
-      flash[:error] = "You must be logged in to add a new user"
-      redirect_to "/sessions/new"
-    end
-  end
-
   before_filter :require_login
 
 end
