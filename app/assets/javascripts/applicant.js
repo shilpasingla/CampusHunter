f852bec218453660b465c766c14550ae4e24854b
vinay mor <vinaymor002@gmail.com>
2012-10-19 15:13:13 +0530

    move autoSave Script to applicant.js

--- app/assets/javascripts/applicant.js
+++ app/assets/javascripts/applicant.js
@@ -8,6 +8,14 @@ $(document).ready(function(){
      });
 });
 
+function autoSave(object){
+    var value =object.value;
+    var id = object.id;
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("POST","/applicant/auto_save",true);
+    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    xmlhttp.send("score=" + value +"&"+ "id="+id);
+}
 //$("textbox id").onchange(function(){
 //    make an ajax post call;
 //
