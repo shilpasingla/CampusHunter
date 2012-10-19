1925f2e453f3f4b415eb74d6fff6e0ab888613a2
arnav90 <arnavk90@thoughtworks.com>
2012-10-19 17:35:46 +0530

    Arnav/Isha - Fixed broken link to Add User

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ CampusHunter::Application.routes.draw do
 
   root :to => "college#show"
   resources :sessions
+  resources :users
   get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "sign_up" => "users#new", :as => "sign_up"
