64362fa0a96fadd3c6454450be367c51a6cab4cd
shilpa <shilpas@thoughtworks.com>
2012-10-22 09:59:25 +0530

    removed pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,6 @@ class SessionsController < ApplicationController
   def create
     user = User.authenticate(params[:email], params[:password])
     if user
-      require 'pry'
-      binding.pry
       session[:user_id] = user.id
       redirect_to "/college/show_all"
     else
