b5c2625df59ece659ae1f5077c55d93791566f2b
isha <thisisisha@github.com>
2012-10-25 16:36:53 +0530

    download csv method changed

--- app/models/applicants.rb
+++ app/models/applicants.rb
@@ -7,11 +7,11 @@ class Applicants < ActiveRecord::Base
   validates :Name, :RollNo, :presence => true
   validates :Score, :numericality => {:less_than_or_equal_to => 12, :greater_than_or_equal_to => 0}, :allow_blank => true, :allow_nil => true
 
-  def self.to_csv(college_name)
+  def self.to_csv(college_name,cutoff)
     @college = College.find_by_name(college_name)
     CSV.generate do |csv|
       csv << column_names
-      @college.final_pursued().each do |applicant|
+      @college.logic_pursued(cutoff).each do |applicant|
         csv << applicant.attributes.values_at(*column_names)
       end
     end
