728fac42d5f30da5e462af0146f73044799e3535
isha <thisisisha@github.com>
2012-10-25 10:20:05 +0530

    download fixed

--- app/models/applicants.rb
+++ app/models/applicants.rb
@@ -8,9 +8,10 @@ class Applicants < ActiveRecord::Base
   validates :Score, :numericality => {:less_than_or_equal_to => 12, :greater_than_or_equal_to => 0}, :allow_blank => true, :allow_nil => true
 
   def self.to_csv(college_name,cutoff)
+    @college = College.find_by_name(college_name)
     CSV.generate do |csv|
       csv << column_names
-      get_pursued(cutoff,college_name).each do |applicant|
+      @college.pursued(cutoff).each do |applicant|
         csv << applicant.attributes.values_at(*column_names)
       end
     end
