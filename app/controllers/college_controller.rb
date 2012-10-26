7c23b3326b3d2b9c7dbfe22bcd64bcb2ad5f7d6a
vinay mor <vinaymor002@gmail.com>
2012-10-26 13:21:21 +0530

    added confirmation for remove college .....

--- app/controllers/college_controller.rb
+++ app/controllers/college_controller.rb
@@ -1,7 +1,7 @@
 class CollegeController < ApplicationController
   before_filter :require_login
-  #helper_method :new
-  helper_method :load_csv_to_database
+  include CollegeHelper
+  #helper_method :load_csv_to_database
 
   def new
   end
@@ -41,31 +41,4 @@ class CollegeController < ApplicationController
     redirect_to '/college/show'
   end
 
-  private
-
-  def load_csv_to_database(file_name,college_name)
-
-    CSV.new( file_name.tempfile, :headers => true, :col_sep=> "," ).each do |row|
-      Applicants.create!(
-          :Name => row[0],
-          :RollNo => row[1],
-          :Gender => row[2],
-          :EmailAdd => row[3],
-          :PhoneNo => row[4],
-          :Qualification => row[5],
-          :Branch => row[6],
-          :Percentage => row[7],
-          :Score => "",
-          :CodePairing => "",
-          :PairingStatus =>"",
-          :SecondTech =>"",
-          :FirstTech => "",
-          :Role => "",
-          :FirstStatus => "",
-          :Result => "",
-          :Comment => "",
-          :college => college_name
-      )
-    end
-  end
 end
