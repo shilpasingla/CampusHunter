b30f1e00fedbf357da9d1cbec24c93a881cc1536
isha <thisisisha@github.com>
2012-10-30 12:57:34 +0530

    final

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,6 +1,5 @@
 Given /^I am on the login page/ do
   visit "/sessions/new"
-  #save_and_open_page
 end
 
 When /^I fill in email with "(.*)"$/ do |arg2|
@@ -10,18 +9,9 @@ end
 When /^I fill in password with "(.*)"$/ do |arg2|
   fill_in 'password', :with => arg2
   click_button 'Login'
-  #page.should have_css('h1', :text => 'Listing colleges')
-  save_and_open_page
-  #page.should have
 end
 
-#And /Click "(.*)" button/ do |arg|
-#click_button arg
-#end
-
 Then /^I should be directed to the home page/ do
-  #page.should have_css('h1', :text => 'Listing colleges')
-  #visit "/college/show"
-  #save_and_open_page
-  pending
+  page.should have_css('h1', :text => 'Listing colleges')
+  save_and_open_page
 end
