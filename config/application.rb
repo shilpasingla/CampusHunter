2cacf2f90fff1dc9cca524d47b078fa968d773ac
Rashi Phuloria <rashiph@thoughtworks.com>
2012-11-06 12:12:57 +0530

    nav tabs added

--- config/application.rb
+++ config/application.rb
@@ -52,7 +52,7 @@ module CampusHunter
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
-    config.assets.precompile += %w(*.js *.css)
+    #config.assets.precompile += %w(*.js *.css)
     config.serve_static_assets = true
 
 
