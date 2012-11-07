fd1e68abe0df776fc75071a7a12cc5afc9e7f6ff
Rashi Phuloria <rashiph@thoughtworks.com>
2012-11-07 18:24:38 +0530

    Added client side validation and removed it from server side.
    Checked college name and applicants file fields are not blank.

--- app/controllers/college_controller.rb
+++ app/controllers/college_controller.rb
@@ -4,43 +4,37 @@ class CollegeController < ApplicationController
   #helper_method :load_csv_to_database
 
   def new
+
     render :layout => "sessions"
   end
 
   def create
 
-    if params[:import] == nil
-      flash[:error] = "Please select a csv file."
-      render 'college/new'
-
-    else if College.where(:name => params[:name]).count == 0
-      load_csv_to_database params[:import],params[:name]
-      @college = College.new(:name => params[:name],:numberofapplicant => Applicants.where(:college => params[:name]).count)
+    if College.where(:name => params[:name]).count == 0
+      load_csv_to_database params[:import], params[:name]
+      @college = College.new(:name => params[:name],
+                             :numberofapplicant => Applicants.where(:college => params[:name]).count)
       @college.save
       redirect_to "/applicant/show/#{params[:name]}"
-
     else
-      @message =  "College name already exists"
-      render 'college/new'
-    end
-
+      @message = "College name already exists"
+      render "college/new"
     end
   end
 
-
   def show
-    @colleges = Kaminari.paginate_array(College.all).page(params[:page]).per(10)
 
+    @colleges = Kaminari.paginate_array(College.all).page(params[:page]).per(10)
     respond_to do |format|
-      format.html { render 'college/show'}
+      format.html { render 'college/show' }
       format.json { render json: @colleges }
     end
   end
 
   def delete
+
     Applicants.delete_all(:college => params[:collegename])
     College.delete_all(:name => params[:collegename])
     redirect_to '/college/show'
   end
-
 end
