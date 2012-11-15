df6f174415414593bf98d477d2778f842a0540dd
arnav90 <arnavk90@thoughtworks.com>
2012-11-15 16:42:20 +0530

    Arnav - Schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121106114311) do
+ActiveRecord::Schema.define(:version => 20121115095558) do
 
   create_table "applicants", :force => true do |t|
     t.string   "Name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20121106114311) do
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
     t.integer  "cutoff"
+    t.string   "poolName"
   end
 
   create_table "users", :force => true do |t|
