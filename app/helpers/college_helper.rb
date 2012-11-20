5a36f830ab21d032de98dbf187af57e712e859cc
isha <thisisisha@github.com>
2012-11-20 11:58:13 +0530

    no of colleges n aplicants

--- app/helpers/college_helper.rb
+++ app/helpers/college_helper.rb
@@ -19,23 +19,28 @@ module CollegeHelper
 
   private
   def load_pool_to_database(file_name, pool_name)
+    numberOfColleges = 0
+    numberOfApplicants = 0
     @col = []
     CSV.new(file_name.tempfile, :headers => true).each do |row|
       hash = row.to_hash
       collegename = hash["college"]
       hash.delete("college")
-      @col = College.where(:name => collegename, :poolName => pool_name)
-      if(@col == [])
-        @col = College.create!(:name => collegename, :poolName => pool_name, :numberofapplicant => 0, :cutoff => 0)
-        end
-      app = Applicants.create!(hash)
       colleges = College.find_all_by_name(collegename)
       colleges.each do |college|
         if(college.poolName == pool_name)
-          app.update_attribute(:collegeId, college.id)
+          @col = college
+        end
       end
-    end
-  end
+      if(@col == [])
+        numberOfColleges = numberOfColleges + 1
+        @col = College.create!(:name => collegename, :poolName => pool_name, :numberofapplicant => 0, :cutoff => 0)
+        end
+      app = Applicants.create!(hash)
+      numberOfApplicants = numberOfApplicants +1
+          app.update_attribute(:collegeId, @col.id)
+      end
+    Pool.find_by_name(pool_name).update_attributes(:numberOfColleges => numberOfColleges, :numberOfApplicants => numberOfApplicants)
   end
 end
 
