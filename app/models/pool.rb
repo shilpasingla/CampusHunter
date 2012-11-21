299183cdf486fa8fc626e18d20aa47d1758ec4d2
isha <thisisisha@github.com>
2012-11-21 16:00:17 +0530

    validation

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,4 +1,6 @@
 class Pool < ActiveRecord::Base
   attr_accessible :cutoff, :name, :numberOfColleges, :numberOfApplicants
+  validates :numberOfApplicants,:numberOfColleges, :numericality => {:only_integer => true}
+  validates :name, :presence => true
 
 end
