cb78d0affe8c42c6300532f450040eb9f98da8bc
arnav90 <arnavk90@thoughtworks.com>
2012-11-22 10:18:56 +0530

    Arnav/Vinay - Removing validation for presence of Roll No

--- app/models/applicants.rb
+++ app/models/applicants.rb
@@ -4,7 +4,7 @@ class Applicants < ActiveRecord::Base
 
   attr_accessible :PhoneNo, :RollNo, :Gender, :EmailAdd, :Qualification, :Percentage, :Branch, :CodePairing, :Comment, :FirstStatus, :FirstTech, :Name, :PairingStatus, :Result, :Role, :Score, :SecondTech, :collegeId
 
-  validates :Name, :RollNo, :presence => true
+  validates :Name, :presence => true
   validates :Score, :numericality => {:less_than_or_equal_to => 12, :greater_than_or_equal_to => 0}, :allow_blank => true, :allow_nil => true
 
   def self.to_csv(college_id, cutoff, round)
