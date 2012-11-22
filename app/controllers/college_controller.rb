4ce0c263b5ba7ab7d9374509300721099f8fd959
arnav90 <arnavk90@thoughtworks.com>
2012-11-22 11:05:30 +0530

    Arnav/Vinay - Removing pagination from college show

--- app/controllers/college_controller.rb
+++ app/controllers/college_controller.rb
@@ -24,7 +24,7 @@ class CollegeController < ApplicationController
   def show
     @colleges = []
     if !params[:name].nil?
-    @colleges = Kaminari.paginate_array(College.find_all_by_poolName(params[:name])).page(params[:page]).per(10)
+    @colleges = Kaminari.paginate_array(College.find_all_by_poolName(params[:name])).page(params[:page]).per(100)
     else
       colleges = College.all
       colleges.each do |college|
@@ -32,7 +32,7 @@ class CollegeController < ApplicationController
           @colleges << college
         end
       end
-    @colleges = Kaminari.paginate_array(@colleges).page(params[:page]).per(10)
+    @colleges = Kaminari.paginate_array(@colleges).page(params[:page]).per(100)
     respond_to do |format|
       format.html { render 'college/show' }
       format.json { render json: @colleges }
